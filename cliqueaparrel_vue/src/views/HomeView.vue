a569818ca95c109fa142f1101e4f9fc8bbb80cb1
Matekoa <matekoa@gmail.com>
2022-08-15 18:22:27 +0200

    Home Page Title and slogan on page.

--- cliqueaparrel_vue/src/views/HomeView.vue
+++ cliqueaparrel_vue/src/views/HomeView.vue
@@ -7,29 +7,42 @@
           <p class="subtitle"> Fashion at your convenience</p>
         </div>
       </section>
+
+
       <div class="columns is-multiline">
         <div class="column is-12">
           <h2 class="is-size-2 has-text-centered">Latest Products</h2>
         </div>
-
-        <!--Bind unique key for each product-->
-        <div class="column is-3" 
-             v-for="products in latestProducts"
-             v-bind:key="product.id"
-             ></div>
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail" alt="Product Thumbnail">
-          </figure>
-
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-          View Details
-        </div>
       </div>
+
+      <!--Bind unique key for each product-->
+      <div class="column is-3" 
+            v-for="products in latestProducts"
+            v-bind:key="products.id"
+            >
+
+            <div class="box">
+                <figure class="image mb-4">
+                    <img v-bind:src="products.get_thumbnail" alt="Product for sale">
+                </figure>
+                <h3 class="is-size-4">{{ products.name }}</h3>
+                <p class="is-size-6 has-text-grey">${{ products.price }}</p>
+
+                View Details
+            </div>
+      </div>
+
+
+        
   </div>
 </template>
 
 <script>
+// Use Axios To create a function that gets data from the backend
+  export default {
+    name: 'Home',
+    components: {
+      
+    }
+  }
 </script>
