10fe3de1cba3b96fe44e5f2ef247fde47ebe8ffc
Matekoa <matekoa@gmail.com>
2022-08-16 21:00:34 +0200

    Set up vuex/state for shopping cart

--- cliqueaparrel_vue/src/store/index.js
+++ cliqueaparrel_vue/src/store/index.js
@@ -2,7 +2,18 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart:{
+
+      items: [],
+
+    },
+
+    isAuthenticated: false,
+    token: '',
+    isLoading: false
+
   },
+
   getters: {
   },
   mutations: {
