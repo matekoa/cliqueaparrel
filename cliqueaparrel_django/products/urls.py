e4c314d69297134cbdf7e1bba41f9e9e4f50ed46
Matekoa <matekoa@gmail.com>
2022-08-16 20:25:07 +0200

    Create viewset for django

--- cliqueaparrel_django/products/urls.py
+++ cliqueaparrel_django/products/urls.py
@@ -5,4 +5,5 @@ from products import views
 
 urlpatterns = [
     path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view())
 ]
--- cliqueaparrel_django/products/views.py
+++ cliqueaparrel_django/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -14,7 +14,16 @@ class LatestProductsList(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
+#View set for product details
 class ProductDetail((APIView)):
     def get_object(self, category_slug, product_slug):
         try:
-            return Products.objects.filter(category__slug=category_slug).get(slug=product_slug)
+            return Products.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        
+        except Products.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug,product_slug, format=None):
+        products = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(products)
+        return Response(serializer.data)
