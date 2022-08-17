376bb109c12978e66bf6bf5970e1cdfcba5c0464
Matekoa <matekoa@gmail.com>
2022-08-17 18:20:27 +0200

    cart page. To fix: table not displaying in cart.

---
+++ cliqueaparrel_vue/src/components/CartItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <tr>
+    <td><router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link></td>
+    <td>$ {{ item.product.price}}</td>
+    <td> {{ item.quantity }}</td>
+
+    <td>$ {{getItemTotal(item).toFixed(2)}}</td>
+    <td><button class="delete"></button></td>
+  </tr>
+</template>
+
+
+<script>
+// Use Axios To create a function that gets data from the backend
+import axios from 'axios'
+
+  export default {
+    name: 'CartItem',
+    props: {
+        initialItem: Object
+    },
+
+    data(){
+        return{
+            item: this.initialItem
+        }
+    },
+
+    methods: {
+        getItemTotal(item){
+            return item.quantity * item.product.price
+        },
+    }
+  }
+</script>
--- cliqueaparrel_vue/src/router/index.js
+++ cliqueaparrel_vue/src/router/index.js
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -44,6 +45,12 @@ const routes = [
     path: '/search',
     name: 'Search',
     component: Search
+  },
+
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   },
 
 ]
